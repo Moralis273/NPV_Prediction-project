855207e10c096b8cb3b1a9637a5f26f41ca7b1c3
Ruslan_Khaydarshin <chaydarshin@mail.ru>
2025-09-30 10:50:58 +0300

    coorect app, auto reload of model

--- app.py
+++ app.py
@@ -94,6 +94,7 @@ async def model_info():
     except Exception as e:
         return {"error": str(e)}
 
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8001)
